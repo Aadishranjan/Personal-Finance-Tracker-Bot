55a264a5ab78963ddee17fd75af42879b735750c
Aadishranjan <aadishranjan35@gmail.com>
2025-12-11 16:50:35 +0530

    add image in start command

--- handlers/start.py
+++ handlers/start.py
@@ -4,13 +4,21 @@ from db import add_user
 # -----------------------------
 # Start command
 # -----------------------------
+
 async def start(msg: types.Message):
-    # Check if user already exists
+    # Save user to database
     add_user(msg.from_user.id, msg.from_user.first_name, msg.from_user.username)
 
+    # Send image first
+    await msg.answer_photo(
+        photo="https://i.ibb.co/spBqv7qX/Budgely.png",
+        caption="👋 Welcome to *Budgely* — Your Personal Finance Tracker Bot!",
+        parse_mode="Markdown"
+    )
+
+    # Then send instructions
     await msg.reply(
-        "👋 Welcome to Personal Finance Tracker Bot!\n\n"
-        "Use these commands:\n"
+        "Use these commands:\n\n"
         "💸 Add Expense → /expense 500 food\n"
         "💰 Add Saving → /save 1000\n"
         "📊 Monthly Summary → /summary\n"
